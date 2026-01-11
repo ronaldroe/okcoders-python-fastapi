2210fd73f19d5c1a2d2b5cb589c57d27df4b340e
Ron Roe <ronald.roe@gmail.com>
2026-01-11 17:30:49 +0000

    committing so I can go fix something on lesson 1. main.py is complete here for the health check

---
+++ lessons/lesson-2.md
@@ -0,0 +1,1 @@
+# FastAPI Microservice Lesson 2
--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+import logging
 
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
+from api.health import health_router
+
+app = FastAPI(
+    docs_url="/user/docs",
+    redoc_url="/user/redoc",
+    openapi_url="/user/openapi.json",
+    title="User Service API",
+    version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(health_router, prefix="/health", tags=["Health"])
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    logger.error(f"Unhandled exception: {exc}", exc_info=True)
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Internal Server Error"},
+    )
